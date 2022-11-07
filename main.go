6b5a28e075da3bac01f3c386594539ef5108e428
HaoweiChang <howard3317@gmail.com>
2022-11-08 02:18:55 +0800

    finish go matrix

--- main.go
+++ main.go
@@ -7,86 +7,96 @@ import (
 )
 
 const (
-	mi = 500
-	mj = 800
+	mi = 50
+	mj = 80
 )
 
 var matrixA [mi][mj]float32
 var matrixB [mj][mi]float32
-var matrixC [mi][mi]float32
+var forloopMatrix [mi][mi]float32
+var multithreadMatrix1 [mi][mi]float32
+var multithreadMatrix2 [mi][mi]float32
 
 func init() {
 	start := time.Now()
-	initmatrixA(&matrixA)
-	initmatrixB(&matrixB)
+	initMatrixA(&matrixA)
+	initMatrixB(&matrixB)
 	duration := time.Since(start).Milliseconds()
 	fmt.Printf("初始化陣列計算所花時間%dms\n", duration)
 }
 
 func main() {
-	start := time.Now()
-	forLoop()
-	duration := time.Since(start).Milliseconds()
-	fmt.Printf("for-loop計算所花時間%dms\n", duration)
-	start = time.Now()
-	multiplethread1()
-	duration = time.Since(start).Milliseconds()
-	fmt.Printf("multiplethread1計算所花時間%dms\n", duration)
-	start = time.Now()
-	multiplethread2()
-	duration = time.Since(start).Milliseconds()
-	fmt.Printf("multiplethread2計算所花時間%dms\n", duration)
+	for i := 1; i <= 3; i++ {
+		fmt.Printf("第%d次執行\n", i)
+		start := time.Now()
+		forLoop(&forloopMatrix)
+		duration := time.Since(start).Milliseconds()
+		fmt.Printf("for-loop計算所花時間%dms\n", duration)
+
+		start = time.Now()
+		multiplethread1(&multithreadMatrix1)
+		duration = time.Since(start).Milliseconds()
+		fmt.Printf("multiplethread1計算所花時間%dms\n", duration)
+
+		start = time.Now()
+		multiplethread2(&multithreadMatrix2)
+		duration = time.Since(start).Milliseconds()
+		fmt.Printf("multiplethread2計算所花時間%dms\n\n", duration)
+		time.Sleep(500 * time.Millisecond)
+	}
+	fmt.Print("請按下任意鍵離開")
 	fmt.Scanln()
 }
 
-func forLoop() [mi][mi]float32 {
-	for i := 0; i < len(matrixC); i++ {
-		for j := 0; j < len(matrixC[0]); j++ {
-			matrixC[i][j] = createMatrixC(i, j)
+func forLoop(mtx *[mi][mi]float32) {
+	for i := 0; i < len(mtx); i++ {
+		for j := 0; j < len(mtx[0]); j++ {
+			mtx[i][j] = createMatrixC(i, j)
 		}
 	}
-	return matrixC
 }
 
-func multiplethread1() [mi][mi]float32 {
+func multiplethread1(mtx *[mi][mi]float32) {
 	var wg sync.WaitGroup
-	for i := 0; i < len(matrixC); i++ {
-		for j := 0; j < len(matrixC[0]); j++ {
+	for i := 0; i < len(mtx); i++ {
+		for j := 0; j < len(mtx[0])/50; j++ {
 			wg.Add(1)
 			go func(i int, j int) {
 				defer wg.Done()
-				matrixC[i][j] = createMatrixC(i, j)
+				jstart := j * 50
+				jend := jstart + 50
+				for j := jstart; j < jend; j++ {
+					mtx[i][j] = createMatrixC(i, j)
+				}
 			}(i, j)
 		}
 	}
 	wg.Wait()
-	return matrixC
 }
 
-func multiplethread2() [mi][mi]float32 {
+func multiplethread2(mtx *[mi][mi]float32) {
 	var wg sync.WaitGroup
-	for i := 0; i < 5; i++ {
-		for j := 0; j < 2; j++ {
+	for i := 0; i < len(mtx)/10; i++ {
+		for j := 0; j < len(mtx)/25; j++ {
 			wg.Add(1)
 			go func(i int, j int) {
 				defer wg.Done()
-				istart := i * (len(matrixC) / 5)
-				iend := istart + (len(matrixC) / 5)
-				jstart := j * (len(matrixC[0]) / 2)
-				jend := jstart + (len(matrixC[0]) / 2)
+				istart := i * 10
+				iend := istart + 10
+				jstart := j * 25
+				jend := jstart + 25
 				for i := istart; i < iend; i++ {
 					for j := jstart; j < jend; j++ {
-						matrixC[i][j] = createMatrixC(i, j)
+						mtx[i][j] = createMatrixC(i, j)
 					}
 				}
 			}(i, j)
 		}
 	}
 	wg.Wait()
-	return matrixC
 }
 
-func initmatrixA(mtx *[mi][mj]float32) {
+func initMatrixA(mtx *[mi][mj]float32) {
 	for i := 1; i <= len(mtx); i++ {
 		for j := 1; j <= len(mtx[0]); j++ {
 			mtx[i-1][j-1] = 6.6*float32(i) - 3.3*float32(j)
@@ -94,7 +104,7 @@ func initmatrixA(mtx *[mi][mj]float32) {
 	}
 }
 
-func initmatrixB(mtx *[mj][mi]float32) {
+func initMatrixB(mtx *[mj][mi]float32) {
 	for i := 1; i <= len(mtx); i++ {
 		for j := 1; j <= len(mtx[0]); j++ {
 			mtx[i-1][j-1] = 100 + 2.2*float32(i) - 5.5*float32(j)
